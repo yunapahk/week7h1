308c7048b018de17ecc1827d62b454ffaf5db5ad
Yuna Pahk <yunapahk@gmail.com>
2023-06-06 19:26:49 -0700

    Greeting express application created.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+
+// GREETINGS
+app.get("/greeting", (req, res) => {
+    res.send("Hello, stranger")
+})
+
+app.get("/greeting/:name", (req, res) => {
+    const name = req.params.name
+    res.send(`<h1>${name} Hey! What's up?</h1>`)
+})
+
+app.listen(3000,() => {
+    console.log("Hello, LA. I'm listening...")
+})
+
+// Tip Calculator
